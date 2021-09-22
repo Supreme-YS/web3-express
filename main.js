5043cb87f60b324543a3d7a29441ce151f28ea24
Supreme-YS <dudtjr1225@gmail.com>
2021-09-22 18:23:47 +0900

    Express Version CRUD Done :D

--- main.js
+++ main.js
@@ -5,30 +5,30 @@ var path = require('path');
 var qs = require('querystring');
 var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
- 
+
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
+app.get('/', function (request, response) {
+  fs.readdir('./data', function (error, filelist) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(filelist);
     var html = template.HTML(title, list,
       `<h2>${title}</h2>${description}`,
       `<a href="/create">create</a>`
-    ); 
+    );
     response.send(html);
   });
 });
- 
-app.get('/page/:pageId', function(request, response) { 
-  fs.readdir('./data', function(error, filelist){
+
+app.get('/page/:pageId', function (request, response) {
+  fs.readdir('./data', function (error, filelist) {
     var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
       var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
       var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
+        allowedTags: ['h1']
       });
       var list = template.list(filelist);
       var html = template.HTML(sanitizedTitle, list,
@@ -44,9 +44,9 @@ app.get('/page/:pageId', function(request, response) {
     });
   });
 });
- 
-app.get('/create', function(request, response){
-  fs.readdir('./data', function(error, filelist){
+
+app.get('/create', function (request, response) {
+  fs.readdir('./data', function (error, filelist) {
     var title = 'WEB - create';
     var list = template.list(filelist);
     var html = template.HTML(title, list, `
@@ -63,27 +63,27 @@ app.get('/create', function(request, response){
     response.send(html);
   });
 });
- 
-app.post('/create_process', function(request, response){
+
+app.post('/create_process', function (request, response) {
   var body = '';
-  request.on('data', function(data){
-      body = body + data;
+  request.on('data', function (data) {
+    body = body + data;
   });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
+  request.on('end', function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.writeHead(302, { Location: `/?id=${title}` });
+      response.end();
+    })
   });
 });
- 
-app.get('/update/:pageId', function(request, response){
-  fs.readdir('./data', function(error, filelist){
+
+app.get('/update/:pageId', function (request, response) {
+  fs.readdir('./data', function (error, filelist) {
     var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
       var title = request.params.pageId;
       var list = template.list(filelist);
       var html = template.HTML(title, list,
@@ -106,62 +106,40 @@ app.get('/update/:pageId', function(request, response){
     });
   });
 });
- 
-app.post('/update_process', function(request, response){
+
+app.post('/update_process', function (request, response) {
   var body = '';
-  request.on('data', function(data){
-      body = body + data;
+  request.on('data', function (data) {
+    body = body + data;
   });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect(`/?id=${title}`);
-        })
-      });
+  request.on('end', function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+        response.redirect(`/?id=${title}`);
+      })
+    });
   });
 });
 
-app.post('/delete_process', function(request, response){
+app.post('/delete_process', function (request, response) {
   var body = '';
-  request.on('data', function(data){
-      body = body + data;
+  request.on('data', function (data) {
+    body = body + data;
   });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/');
-      })
+  request.on('end', function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+      response.redirect('/');
+    })
   });
 });
- 
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
-
-//       });
-//     } else if(pathname === '/delete_process'){
-//       var body = '';
-//       request.on('data', function(data){
-//           body = body + data;
-//       });
-//       request.on('end', function(){
-//           var post = qs.parse(body);
-//           var id = post.id;
-//           var filteredId = path.parse(id).base;
-//           fs.unlink(`data/${filteredId}`, function(error){
-//             response.writeHead(302, {Location: `/`});
-//             response.end();
-//           })
-//       });
-//     } else {
-//       response.writeHead(404);
-//       response.end('Not found');
-//     }
-// });
-// app.listen(3000);
