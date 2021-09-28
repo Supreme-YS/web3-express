9847d19dfa7f09b195d04fefa633b69745983e5c
Supreme-YS <dudtjr1225@gmail.com>
2021-09-28 18:07:16 +0900

    need to fix error

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var path = require('path');
 var qs = require('querystring');
-var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
 var compression = require('compression');
 
+/* routes */
+var topicRoute = require('./routes/topic');
+app.use('/topic', topicRoute); // /topic으로 시작하는 주소들에게 topicRoute라는 미들웨어를 적용하겠다.는 뜻
+
+/* compression */
 app.use(compression());
 
 /* how to serve static files */
@@ -40,150 +43,6 @@ app.get('/', function (request, response) {
     `<a href="/topic/create">create</a>`
   );
   response.send(html);
-});
-
-app.get('/topic/create', function (request, response) {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
-
-app.post('/topic/create_process', function (request, response) {
-  console.log(request.list);
-  // body-parser를 사용한 코드 변경
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.redirect(`/topic/${title}`);
-  })
-  /*
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.writeHead(302, { Location: `/?id=${title}` });
-      response.end();
-    })
-  }); */
-});
-
-app.get('/topic/update/:pageId', function (request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/topic/create">create</a> <a href="/topic/update?id=${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post('/topic/update_process', function (request, response) {
-
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.redirect(`/topic/${title}`);
-    })
-  });
-
-  /*
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/?id=${title}`);
-      })
-    });
-  }); */
-});
-
-app.post('/topic/delete_process', function (request, response) {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    response.redirect('/');
-  })
-
-  /*
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function (error) {
-      response.redirect('/');
-    })
-  }); */
-});
-app.get('/topic/:pageId', function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
 });
 
 /* 에러처리 */
