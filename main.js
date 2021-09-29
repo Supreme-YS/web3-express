1c8d03926facb4e405bde492a59d2d450ef568f4
Supreme-YS <dudtjr1225@gmail.com>
2021-09-29 16:57:19 +0900

    adjusting helmet

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
-var compression = require('compression')
+var compression = require('compression');
+var helmet = require('helmet');
  
 var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
- 
+
+app.use(helmet())
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
